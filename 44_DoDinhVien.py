62a8c6b6ea0266b4d661548e303db3be364d52c7
duvu9565-tech <duvu9565@gmail.com>
2025-10-20 19:51:11 +0700

    add student

---
+++ 44_DoDinhVien.py
@@ -0,0 +1,18 @@
+# student_manager.py
+
+# Danh sách để lưu thông tin các sinh viên.
+# Mỗi sinh viên là một dictionary.
+student_list = []
+
+def add_student(name, year_of_birth, address):
+    new_student = {
+        'name': name,
+        'year_of_birth': year_of_birth,
+        'address': address
+    }
+    student_list.append(new_student)
+   """ print(f"Da them sinh vien {name} thanh cong.") """
+
+def print_student_list():
+    for student in student_list:
+        print(f"Tên: {student['name']}, Năm sinh: {student['year_of_birth']}, Địa chỉ: {student['address']}")
